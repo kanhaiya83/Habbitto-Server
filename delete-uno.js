7cadec1cf85f2db866f3bac3f0f135bbe06eed74
kanhaiya83 <76777058+kanhaiya83@users.noreply.github.com>
2022-01-27 14:26:27 +0530

    Delete delete-uno.js

--- delete-uno.js
+++
@@ -1,4 +0,0 @@
-const { Game, Card, Values, Colors } = require('uno-engine');
-const players = ['Player 1', 'Player 2']; // maximum 10 players with unique names
-const game = Game(players); // initialize the game
-game.newGame();
